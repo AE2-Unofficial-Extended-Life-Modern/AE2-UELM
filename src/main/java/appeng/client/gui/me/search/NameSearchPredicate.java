50dd8618ec145766338a5b88e9f73913faecc3cf
trulyno <78795220+trulyno@users.noreply.github.com>
2026-06-23 13:22:22 +0000

    strip formatting characters from item/fluid names before search

--- src/main/java/appeng/client/gui/me/search/NameSearchPredicate.java
+++ src/main/java/appeng/client/gui/me/search/NameSearchPredicate.java
@@ -2,12 +2,15 @@ package appeng.client.gui.me.search;
 
 import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
 import appeng.api.stacks.AEKey;
 import appeng.menu.me.common.GridInventoryEntry;
 
 final class NameSearchPredicate implements Predicate<GridInventoryEntry> {
     private final String term;
+
+    private static final Pattern FORMATTING_CODES = Pattern.compile("§.");
 
     public NameSearchPredicate(String term) {
         this.term = term.toLowerCase();
@@ -16,7 +19,7 @@ final class NameSearchPredicate implements Predicate<GridInventoryEntry> {
     @Override
     public boolean test(GridInventoryEntry gridInventoryEntry) {
         AEKey entryInfo = Objects.requireNonNull(gridInventoryEntry.getWhat());
-        String displayName = entryInfo.getDisplayName().getString();
+        String displayName = FORMATTING_CODES.matcher(entryInfo.getDisplayName().getString()).replaceAll("");
         return displayName.toLowerCase().contains(term);
     }
 }
