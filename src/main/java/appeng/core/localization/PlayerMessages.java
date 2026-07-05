48824abf15203ce44877dd50ca81c6d1ac2df7de
Kolja <contact@kolja.lu>
2026-07-05 13:11:46 +0200

    message player when a craft finished

--- src/main/java/appeng/core/localization/PlayerMessages.java
+++ src/main/java/appeng/core/localization/PlayerMessages.java
@@ -25,6 +25,7 @@ public enum PlayerMessages implements LocalizationEnum {
     ChannelModeCurrent("Current channel mode: %s"),
     ClickToShowDetails("Click to show details"),
     ClickToTeleport("Click to teleport into plot"),
+    CraftJobFinished("Crafting of %s * [%s] finished in %s."),
     CommunicationError("Error Communicating with Network."),
     DeviceNotLinked("Device is not linked."),
     LinkedNetworkNotFound("Linked network cannot be found"),
